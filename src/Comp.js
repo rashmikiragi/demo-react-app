c4ace1a810e72780143824284edc98649540dfee
Vishal <vm925623@gmail.com>
2021-08-04 18:38:00 +0530

    is doing test run

---
+++ src/Comp.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Comp extends Component{
+    render(){
+        return <h1>Hello World</h1>;
+    }
+}
+
+export default Comp;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Comp from './Comp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Comp/>
   </React.StrictMode>,
   document.getElementById('root')
 );
